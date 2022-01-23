cac274f650208481dfca2f7b378896c2cf9827ea
Allen Day <allenday@allenday.com>
2022-01-23 12:33:06 +0800

    simplify server to static serve a dir + 301 on /

---
+++ appengine/src/app.js
@@ -0,0 +1,21 @@
+const http = require('http');
+const fs = require('fs');
+const finalhandler = require('finalhandler');
+const serveStatic = require('serve-static');
+const serve = serveStatic("./public");
+
+let port = process.env.PORT || 3000
+
+var server = http.createServer(function(req, res) {
+  if (req.url == '/') {
+    res.writeHead(301, {'Location': '/index.html'});
+    res.end();
+    return;
+  }
+
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
+
+server.listen(port);
+console.log('node server running on port '+port);
