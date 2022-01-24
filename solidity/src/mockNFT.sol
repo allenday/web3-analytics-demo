5915a30450ee37634ac1edc4f6bcc9c371db9a7f
Allen Day <allenday@allenday.com>
2022-01-24 13:56:37 +0800

    mock

--- solidity/src/mockNFT.sol
+++ solidity/src/mockNFT.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.11;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol"; //totalsupply()
 
-contract IdkNFT is ERC721, ERC721Enumerable {
-    constructor() ERC721("idk", "IDK") {}
+contract mockNFT is ERC721, ERC721Enumerable {
+    constructor() ERC721("mock", "MOCK") {}
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC721,ERC721Enumerable) {}
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721,ERC721Enumerable) returns (bool) {}
