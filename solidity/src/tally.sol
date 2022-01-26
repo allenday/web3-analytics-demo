6c588bcc5b894fe21611ec5807de454fc27e2eda
Roads Goji <roads@goji.link>
2022-01-26 01:16:42 +0000

    Possibly-final contract (janky edition).
    Also, HTML changed to dump current vote-stats; ABI bumped; setNotes() demo removed.

--- solidity/src/tally.sol
+++ solidity/src/tally.sol
@@ -1,20 +1,40 @@
 pragma solidity ^0.8.11;
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract Tally { 
+// SPDX-License-Identifier: UNLICENSED
 
-    uint itemCount;
-    mapping( uint => Stats ) itemStats;
+interface TallyMan {
+    event observe(address indexed from, uint256 indexed tokenId);
+    event voteFor(address indexed from, uint256 indexed tokenId);
+    //event voteAgainst(address indexed from, uint256 indexed tokenId);
+}
+
+contract Tally is TallyMan {
+    uint itemCount = 1006;
+    mapping(uint => Stats) public itemStats;
 
     struct Stats {
         uint observations;
         uint hits;
     }
 
-    function init(address collectionAddress) public pure {
-        for(int i = 0; i < collectionAddress.length; i++) {
-            itemStats[i] = Stats(0, 0);
+    function vote (uint256 tkid_for,uint256 tkid_against) public {
+        unchecked {
+            itemStats[tkid_for].hits += 1;
+            itemStats[tkid_for].observations += 1;
+            itemStats[tkid_against].observations += 1;
         }
+        emit voteFor(msg.sender,tkid_for);
+        emit observe(msg.sender,tkid_for);
+        //emit voteAgainst(msg.sender,tkid_against);
+        emit observe(msg.sender,tkid_against);
     }
 
+    function getStats() public returns (Stats[] memory) {
+        //return itemStats; // that would be nice, but..
+        Stats[] memory tub = new Stats[](itemCount);
+        for(uint ii = 0; ii != itemCount; ii++) {
+            tub[ii] = itemStats[ii];
+        }
+        return tub;
+    }
 }
