3184eb2b280a06d7f85503e0b208c03a41917351
Si <smurray@fwd.ltd>
2022-03-18 03:41:29 +0000

    firebase: API config.

--- appengine/public/config.js
+++ appengine/public/config.js
@@ -6,15 +6,15 @@
 const clientId = "BJFhyOH7nxD4PRd1CYdSpakfp4Vikf_Knodm0BiS1GuElQN90iXlxh5tS-sIKWjmAqtDfLco0HgEesOtICPjmsM";
 
  // get your firebase config from firebase console
- const firebaseConfig = {
-     apiKey: "AIzaSyCkbfXYqxw7ygo-XxfAt866Yja4jNs31Po",
-     authDomain: "web3auth-firebase.firebaseapp.com",
-     projectId: "web3auth-firebase",
-     storageBucket: "web3auth-firebase.appspot.com",
-     messagingSenderId: "707643268846",
-     appId: "1:707643268846:web:6d92e69726852a38e39dff",
-     measurementId: "G-V631CSBMVY",
- };
+const firebaseConfig = {
+    apiKey: "AIzaSyBYdB3aRi3hQjQ33Ymu2vx12WRvW-BSWz4",
+    authDomain: "web3-analytics-demo.firebaseapp.com",
+    projectId: "web3-analytics-demo",
+    storageBucket: "web3-analytics-demo.appspot.com",
+    messagingSenderId: "344660056820",
+    appId: "1:344660056820:web:87dd1cbe8a75356b185ca1",
+    measurementId: "G-EHJCZHNGP6"
+};
  
 
 // Initialize Firebase
