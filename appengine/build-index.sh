3ccf3e954e972a931a2db51afb96345feab0ab6f
Allen Day <allenday@allenday.com>
2022-09-19 22:32:26 +0800

    matching

--- appengine/build-index.sh
+++ appengine/build-index.sh
@@ -12,8 +12,13 @@ if [ -z ${TOKEN_ADDRESS+x} ]; then
   echo '$TOKEN_ADDRESS is not set'
   exit 1
 fi
+if [ -z ${TOKEN_BUCKET_PREFIX+x} ]; then
+  echo '$TOKEN_BUCKET_PREFIX is not set'
+  exit 1
+fi
 
 cp public/index.html.in public/index.html
 perl -pi -e 's/___GTAG_ID___/$ENV{GTAG_ID}/' public/index.html
 perl -pi -e 's/___CONTRACT_ADDRESS___/$ENV{CONTRACT_ADDRESS}/' public/index.html
 perl -pi -e 's/___TOKEN_ADDRESS___/$ENV{TOKEN_ADDRESS}/' public/index.html
+perl -pi -e 's/___TOKEN_BUCKET_PREFIX___/$ENV{TOKEN_BUCKET_PREFIX}/' public/index.html
